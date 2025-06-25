aa545df96f0bd11483a4e6045bf2117ad2b7a982
Izai Vlad <nortenosma@gmail.com>
2025-06-25 07:39:23 +0300

    my solution after fix

--- app/main.py
+++ app/main.py
@@ -15,8 +15,7 @@ def create_person_list(people: list[dict]) -> list[Person]:
     for person_data in people:
         name = person_data["name"]
         age = person_data["age"]
-        person_obj = Person(name, age)
-        person_instances.append(person_obj)
+        person_instances.append(Person(name, age))
 
     for person_data in people:
         current_person_name = person_data["name"]
@@ -26,7 +25,7 @@ def create_person_list(people: list[dict]) -> list[Person]:
             wife_name = person_data["wife"]
             current_person_obj.wife = Person.people.get(wife_name)
 
-        elif "husband" in person_data and person_data["husband"] is not None:
+        if "husband" in person_data and person_data["husband"] is not None:
             husband_name = person_data["husband"]
             current_person_obj.husband = Person.people.get(husband_name)
 
